80680c9107b4a429cb81c0c2e41aa94428b5b1d7
Maxwell-King <112987440+Maxwell-King@users.noreply.github.com>
2025-03-11 00:00:18 -0400

    Update annotator.cpp

--- annotator.cpp
+++ annotator.cpp
@@ -2,46 +2,43 @@
 
 bool wxAnnotatorApp::OnInit()
 {
-  if (!wxApp::OnInit())
-    return false;
-  
-  std::cerr << "Starting annotator..." << std::endl;
-  
-  wxInitAllImageHandlers();
-            
-  frame = new wxFrame(NULL, wxID_ANY, wxT("Annotator"), wxDefaultPosition, wxDefaultSize, wxDEFAULT_FRAME_STYLE & ~(wxRESIZE_BORDER | wxMAXIMIZE_BOX));
-  chooseJSON();
-  drawPanel = new wxImagePanel(frame, file, date);
-  
-  frame->Show();
-  
-  return true;
+	if (!wxApp::OnInit())
+		return false;
+	std::cerr << "Starting annotator..." << std::endl;
+	
+	wxInitAllImageHandlers();
+	frame = new wxFrame(NULL, wxID_ANY, wxT("Annotator"), wxDefaultPosition, wxDefaultSize, wxDEFAULT_FRAME_STYLE & ~(wxRESIZE_BORDER | wxMAXIMIZE_BOX));
+	chooseJSON();
+	drawPanel = new wxImagePanel(frame, file, date);
+	frame->Show();
+	
+	return true;
 }
 
 
 int wxAnnotatorApp::OnExit()
 {
-  closeJSON();
-  return 0;
+	closeJSON();
+	return 0;
 }
 
 int wxAnnotatorApp::OnRun()
 {
-  int exitcode = wxApp::OnRun();
-  return exitcode;
+	int exitcode = wxApp::OnRun();
+	return exitcode;
 }
 
 void wxAnnotatorApp::OnInitCmdLine(wxCmdLineParser& parser)
 {
-  parser.SetDesc(g_cmdLineDesc);
+	parser.SetDesc(g_cmdLineDesc);
 }
 
 bool wxAnnotatorApp::OnCmdLineParsed(wxCmdLineParser& parser)
 {
-  file = parser.GetParam(0);
-  date = parser.GetParam(1);
+	file = parser.GetParam(0);
+	date = parser.GetParam(1);
 
-  return true;
+	return true;
 }
 
 IMPLEMENT_APP(wxAnnotatorApp)
