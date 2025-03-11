9174dfe1889118f3759857caa83e833a91b06c47
Maxwell-King <112987440+Maxwell-King@users.noreply.github.com>
2025-03-11 00:05:40 -0400

    Update entry.c

--- entry.c
+++ entry.c
@@ -6,12 +6,12 @@
 // 4) train from user data
 
 int main ( int argc, char *argv[] ) {
-  if ( argc < 2 ) {
-    fprintf(stderr, "usage:\theadlinereader <path/to/directory_containing_pictures>");
-    return -1;
-  }
-  set_dir(argv[1]);
-  while (1) {
-    watch_dir(); 
-  }
+	if ( argc < 2 ) {
+		fprintf(stderr, "usage:\theadlinereader <path/to/directory_containing_pictures>");
+		return -1;
+	}
+	set_dir(argv[1]);
+	while (1) {
+		watch_dir(); 
+	}
 }
