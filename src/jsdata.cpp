57b9de05af1d5fff75bb6ca25d50db4da77b7cb0
Maxwell-King <112987440+Maxwell-King@users.noreply.github.com>
2025-03-22 19:14:12 -0400

    Update jsdata.cpp

--- src/jsdata.cpp
+++ src/jsdata.cpp
@@ -5,220 +5,220 @@ struct json js_data;
 int img_id;
 
 void match(char t) {
-  if (t == js_in.peek()) {
-    js_in.get();
-  }
-  else {
-    std::cerr << "Expected " << t << " found " << (char)(js_in.peek()) <<  " at " << js_in.tellg() << std::endl;
-    exit(-1);
-  }
+	if (t == js_in.peek()) {
+		js_in.get();
+	}
+	else {
+		std::cerr << "Expected " << t << " found " << (char)(js_in.peek()) <<	" at " << js_in.tellg() << std::endl;
+		exit(-1);
+	}
 }
 
 void match_string(std::string target) {
-  match('\"');
-  for (int pos = 0; pos < target.length(); pos++) {
-    match(target[pos]);
-  }
-  match('\"');
+	match('\"');
+	for (int pos = 0; pos < target.length(); pos++) {
+		match(target[pos]);
+	}
+	match('\"');
 }
 
 std::string get_string() {
-  std::string name;
-  match('\"');
-  while (js_in.peek() != '\"') {
-    name += js_in.get();
-  }
-  match('\"');
-  return name;
+	std::string name;
+	match('\"');
+	while (js_in.peek() != '\"') {
+		name += js_in.get();
+	}
+	match('\"');
+	return name;
 }
 
 std::string match_number() {
 	std::string num;
-  while (std::isdigit(js_in.peek())) { // no negative numbers
+	while (std::isdigit(js_in.peek())) { // no negative numbers
 		num += js_in.get();
-  }
+	}
 	return num;
 }
 
 void match_annotation(struct json *js_rd) {
-  while (true) {
-    if (js_in.peek() == ']') {
-      match(']');
-      break;
-    }
-    match('{');
-    match_string("area");
-    match(':');
-    js_rd->annotations.append("{\"area\":" + match_number());
-    match(',');
-    match_string("bbox");
-    match(':');
-    match('[');
-    js_rd->annotations.append(",\"bbox\":[" + match_number());
-    match(',');
-    js_rd->annotations.append("," + match_number());
-    match(',');
-    js_rd->annotations.append("," + match_number());
-    match(',');
-    js_rd->annotations.append("," + match_number());
-    match(']');
-    match(',');
-    match_string("category_id");
-    match(':');
-    js_rd->annotations.append("],\"category_id\":" + match_number());
-    match(',');
-    match_string("id");
-    match(':');
-    js_rd->annotations.append(",\"id\":" + match_number());
-    match(',');
-    match_string("image_id");
-    match(':');
-    js_rd->annotations.append(",\"image_id\":" + match_number());
-    match(',');
-    match_string("iscrowd");
-    match(':');
-    js_rd->annotations.append(",\"iscrowd\":" + match_number());
-    match('}');
+	while (true) {
+		if (js_in.peek() == ']') {
+			match(']');
+			break;
+		}
+		match('{');
+		match_string("area");
+		match(':');
+		js_rd->annotations.append("{\"area\":" + match_number());
+		match(',');
+		match_string("bbox");
+		match(':');
+		match('[');
+		js_rd->annotations.append(",\"bbox\":[" + match_number());
+		match(',');
+		js_rd->annotations.append("," + match_number());
+		match(',');
+		js_rd->annotations.append("," + match_number());
+		match(',');
+		js_rd->annotations.append("," + match_number());
+		match(']');
+		match(',');
+		match_string("category_id");
+		match(':');
+		js_rd->annotations.append("],\"category_id\":" + match_number());
+		match(',');
+		match_string("id");
+		match(':');
+		js_rd->annotations.append(",\"id\":" + match_number());
+		match(',');
+		match_string("image_id");
+		match(':');
+		js_rd->annotations.append(",\"image_id\":" + match_number());
+		match(',');
+		match_string("iscrowd");
+		match(':');
+		js_rd->annotations.append(",\"iscrowd\":" + match_number());
+		match('}');
 		js_rd->annotations += '}';
-    js_rd->annotations_size++;
-    if (js_in.peek() == ',') {
-      match(',');
+		js_rd->annotations_size++;
+		if (js_in.peek() == ',') {
+			match(',');
 			js_rd->annotations += ',';
 
-    }
-  }
+		}
+	}
 }
 
 std::string match_date() {
 	std::string str_date;
-  match('\"');
-  str_date.append(match_number());
-  match('-');
-  str_date.append('-' + match_number());
-  match('-');
-  str_date.append('-' + match_number());
-  match(' ');
-  str_date.append(' ' + match_number());
-  match(':');
-  str_date.append(':' + match_number());
-  match(':');
-  str_date.append(':' + match_number());
-  match('\"');
+	match('\"');
+	str_date.append(match_number());
+	match('-');
+	str_date.append('-' + match_number());
+	match('-');
+	str_date.append('-' + match_number());
+	match(' ');
+	str_date.append(' ' + match_number());
+	match(':');
+	str_date.append(':' + match_number());
+	match(':');
+	str_date.append(':' + match_number());
+	match('\"');
 	return str_date;
 }
 
 void match_images(struct json *js_rd) {
-  while (true) {
-    if (js_in.peek() == ']') {
-      match(']');
-      break;
-    }
-    match('{');
-    match_string("date_captured");
-    match(':');
-    js_rd->images.append("{\"date_captured\":\"" + match_date() + "\"");
-    match(',');
-    match_string("file_name");
-    match(':');
+	while (true) {
+		if (js_in.peek() == ']') {
+			match(']');
+			break;
+		}
+		match('{');
+		match_string("date_captured");
+		match(':');
+		js_rd->images.append("{\"date_captured\":\"" + match_date() + "\"");
+		match(',');
+		match_string("file_name");
+		match(':');
 		js_rd->images.append(",\"file_name\":\"" + get_string() + "\"");
-    match(',');
-    match_string("height");
-    match(':');
-    js_rd->images.append(",\"height\":" + match_number());
-    match(',');
-    match_string("id");
-    match(':');
-    js_rd->images.append(",\"id\":" + match_number());
-    match(',');
-    match_string("width");
-    match(':');
-    js_rd->images.append(",\"width\":" + match_number());
-    match('}');
+		match(',');
+		match_string("height");
+		match(':');
+		js_rd->images.append(",\"height\":" + match_number());
+		match(',');
+		match_string("id");
+		match(':');
+		js_rd->images.append(",\"id\":" + match_number());
+		match(',');
+		match_string("width");
+		match(':');
+		js_rd->images.append(",\"width\":" + match_number());
+		match('}');
 		js_rd->images += '}';
-    js_rd->images_size++;
-    if (js_in.peek() == ',') {
-      match(',');
+		js_rd->images_size++;
+		if (js_in.peek() == ',') {
+			match(',');
 			js_rd->images += ',';
-    }
-  }
+		}
+	}
 }
 
 void match_categories(struct json *js_rd) { // must have atleast one category (provide user option?)
-  while (true) {
-    match('{');
-    match_string("id");
-    match(':');
-		int cat_id = std::stoi(match_number());
-    js_rd->categories.append("{\"id\":" + std::to_string(cat_id));
-    match(',');
-    match_string("name");
-    match(':');
-		std::string cat_name = get_string();
+	while (true) {
+		match('{');
+		match_string("id");
+		match(':');
+		const int cat_id = std::stoi(match_number());
+		js_rd->categories.append("{\"id\":" + std::to_string(cat_id));
+		match(',');
+		match_string("name");
+		match(':');
+		const std::string cat_name = get_string();
 		js_rd->categories.append(",\"name\":\"" + cat_name + "\"");
-    js_rd->categories_list.push_back(cat_name);
+		js_rd->categories_list.push_back(cat_name);
 		js_rd->cat_map[cat_name] = cat_id;
-    match('}');
+		match('}');
 		js_rd->categories += '}';
-    js_rd->categories_size++;
-    switch (js_in.peek()) {
-    case ',':
-      match(',');
+		js_rd->categories_size++;
+		switch (js_in.peek()) {
+		case ',':
+			match(',');
 			js_rd->categories += ',';
-      continue;
-    case ']':
-      match(']');
-      goto CATEGORIES_OVER;
-    }
-  }
+			continue;
+		case ']':
+			match(']');
+			goto CATEGORIES_OVER;
+		}
+	}
 	CATEGORIES_OVER:;
 }
 
 void parse(struct json *js_rd, std::string file) {
-  js_in.open(file, std::ifstream::in);
-  match('{');
-  match_string("annotations");
-  match(':');
-  match('[');
-  match_annotation(js_rd); // {"area":396459,"bbox":[15,50,1953,203],"category_id":0,"id":3000,"image_id":0,"iscrowd":0}
-  match(',');
-  match_string("categories"); 
-  match(':');
-  match('[');
-  match_categories(js_rd); // {"id":0,"name":"headline"},{"id":1,"name":"contents"}
-  match(',');
-  match_string("images");
-  match(':');
-  match('[');
-  match_images(js_rd); // {"date_captured":"2025-03-04 22:18:44","file_name":"C:/Users/danie/Pictures/test/img3.jpg","height":1857,"id":8,"width":2660}
-  match('}');
+	js_in.open(file, std::ifstream::in);
+	match('{');
+	match_string("annotations");
+	match(':');
+	match('[');
+	match_annotation(js_rd); // {"area":396459,"bbox":[15,50,1953,203],"category_id":0,"id":3000,"image_id":0,"iscrowd":0}
+	match(',');
+	match_string("categories"); 
+	match(':');
+	match('[');
+	match_categories(js_rd); // {"id":0,"name":"headline"},{"id":1,"name":"contents"}
+	match(',');
+	match_string("images");
+	match(':');
+	match('[');
+	match_images(js_rd); // {"date_captured":"2025-03-04 22:18:44","file_name":"C:/Users/danie/Pictures/test/img3.jpg","height":1857,"id":8,"width":2660}
+	match('}');
 	js_in.close();
 }
 
-void add_image(std::string date_captured, std::string file_name, int h_img, int id, int w_img) {
-  std::string image_obj = "{\"date_captured\":\"" + date_captured +
-    "\",\"file_name\":\"" + file_name +
-    "\",\"height\":" + std::to_string(h_img) +
-    ",\"id\":" + std::to_string(id) +
-    ",\"width\":" + std::to_string(w_img) +
-    "}";
-  if (js_data.images_size != 0) {
+void add_image(const std::string date_captured, const std::string file_name, const int h_img, const int id, const int w_img) {
+	std::string image_obj = "{\"date_captured\":\"" + date_captured +
+		"\",\"file_name\":\"" + file_name +
+		"\",\"height\":" + std::to_string(h_img) +
+		",\"id\":" + std::to_string(id) +
+		",\"width\":" + std::to_string(w_img) +
+		"}";
+	if (js_data.images_size != 0) {
 		js_data.images += ',';
 	}
-  js_data.images.append(image_obj);
+	js_data.images.append(image_obj);
 	js_data.images_size++;
 }
 
-void add_annotation(int area, int bbox_x, int bbox_y, int bbox_w, int bbox_h, int cat_id, int id, int img_id) {
-  std::string annotation_obj = "{\"area\":" + std::to_string(area) +
-    ",\"bbox\":[" + std::to_string(bbox_x) + "," + std::to_string(bbox_y) + "," + std::to_string(bbox_w) + "," + std::to_string(bbox_h) + "]"
-    ",\"category_id\":" + std::to_string(cat_id) +
-    ",\"id\":" + std::to_string(id) +
-    ",\"image_id\":" + std::to_string(img_id) +
-    ",\"iscrowd\":0}";
+void add_annotation(const int area, const int bbox_x, const int bbox_y, const int bbox_w, const int bbox_h, const int cat_id, const int id, const int img_id) {
+	std::string annotation_obj = "{\"area\":" + std::to_string(area) +
+		",\"bbox\":[" + std::to_string(bbox_x) + "," + std::to_string(bbox_y) + "," + std::to_string(bbox_w) + "," + std::to_string(bbox_h) + "]"
+		",\"category_id\":" + std::to_string(cat_id) +
+		",\"id\":" + std::to_string(id) +
+		",\"image_id\":" + std::to_string(img_id) +
+		",\"iscrowd\":0}";
 	if (js_data.annotations_size != 0) {
 		js_data.annotations += ',';
 	}
-  js_data.annotations.append(annotation_obj);
+	js_data.annotations.append(annotation_obj);
 	js_data.annotations_size++;
 }
 
@@ -240,7 +240,7 @@ void json_copy(struct json *dest, const struct json src) {
 void js_close() {
 	std::ofstream js_out(js_data.js_filename, std::fstream::trunc);
 	js_out << "{\"annotations\":[" << js_data.annotations << "],\"categories\":[" << js_data.categories << "],\"images\":[" << js_data.images << "]}";
-  js_out.close();
+	js_out.close();
 }
 
 void js_choose() {
